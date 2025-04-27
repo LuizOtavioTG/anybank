61be312e2d41d2469118e7d46bcb17941907520d
LuizOtavioTG <bc.luizo@gmail.com>
2025-04-27 17:40:44 -0300

    vali: adicionando validação de entradas de dados no formulario

--- src/app/form-nova-trasacao/form-nova-trasacao.component.ts
+++ src/app/form-nova-trasacao/form-nova-trasacao.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-nova-trasacao',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form-nova-trasacao.component.html',
   styleUrl: './form-nova-trasacao.component.css'
 })
 export class FormNovaTrasacaoComponent {
-
+  tipoTransacao = "";
+  valorTransacao = "";
+  aoSubmeter(){
+      console.log(this.valorTransacao);
+      console.log(this.tipoTransacao);
+      this.tipoTransacao = "";
+      this.valorTransacao = "";
+  };
 }
