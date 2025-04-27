ca98c92f6fbaa4125beaf60fac359ab6711af30f
LuizOtavioTG <bc.luizo@gmail.com>
2025-04-27 14:49:12 -0300

    feat: adicionando nova transação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from "./banner/banner.component";
+import { FormNovaTrasacaoComponent } from "./form-nova-trasacao/form-nova-trasacao.component";
 
 @Component({
   selector: 'app-root',
-  imports: [BannerComponent],
+  imports: [BannerComponent, FormNovaTrasacaoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/form-nova-trasacao/form-nova-trasacao.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-nova-trasacao',
+  imports: [],
+  templateUrl: './form-nova-trasacao.component.html',
+  styleUrl: './form-nova-trasacao.component.css'
+})
+export class FormNovaTrasacaoComponent {
+
+}
